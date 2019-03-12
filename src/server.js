586b5bb2c343ab90c667eba784cb36a195c4a55d
magic8baller <lturis@gmail.com>
2019-03-11 21:09:53 -0400

    oops

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-import express from 'express'
 import { json, urlencoded } from 'body-parser'
+import cors from 'cors'
+import express from 'express'
 import morgan from 'morgan'
 import config from './config'
-import cors from 'cors'
-import { signup, signin, protect } from './utils/auth'
-import { connect } from './utils/db'
-import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
 import listRouter from './resources/list/list.router'
+import userRouter from './resources/user/user.router'
+import { protect, signin, signup } from './utils/auth'
+import { connect } from './utils/db'
 
 export const app = express()
 
@@ -25,7 +25,6 @@ app.use('/api', protect)
 app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/list', listRouter)
-
 export const start = async () => {
   try {
     await connect()
